b0bb2e1a42f965496610459423da36f80ddad53c
Yoder <tyoder@Admins-MacBook-Pro-2.local>
2015-06-22 14:06:54 -0500

    Atla install now functional

--- ansible-provision.sh
+++ ansible-provision.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-sudo ansible-galaxy install --force -r requirements.yml && ansible-playbook desktop.yml --ask-sudo-pass --verbose
+sudo ansible-galaxy install --force -r requirements.yml && ansible-playbook desktop.yml --ask-sudo-pass #--verbose
